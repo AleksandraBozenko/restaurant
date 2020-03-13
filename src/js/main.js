d81704cf7a2fd3fd4b10b3eb12fa7c8af9a71459
aleksandrabozenko <aleksandra.b.20018@gmail.com>
2020-03-13 16:03:22 +0100

    Add: Menu card descriptions

--- src/js/main.js
+++ src/js/main.js
@@ -30,3 +30,17 @@ cross.addEventListener('click', (e) =>{
   header.classList.toggle('header--js');
   nav.classList.toggle('nav--js');
 })
+
+// Menu descriptions
+
+const option = document.querySelectorAll('.eat__option--header');
+const description = document.querySelector('.eat__description');
+
+option.forEach(('eat__option--header') =>{
+  option.addEventListener('click',(e) =>{
+    description.classList.toggle('eat__description--js')
+  })
+})
+// option.addEventListener('click', (e) =>{
+//   description.classList.toggle('eat__description--js');
+// })
