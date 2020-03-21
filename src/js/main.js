3b3e74990a05a31d284a1c86521d2cf26e1a903b
unknown <aleksandra.b.20018@gmail.com>
2020-03-21 15:20:40 +0100

    Feat: Menu with links

--- src/js/main.js
+++ src/js/main.js
@@ -21,6 +21,7 @@ const cross = document.querySelector('.nav__close--cross');
 const header = document.querySelector('.header');
 const nav = document.querySelector('.nav');
 
+
 hamburger.addEventListener('click', (e) =>{
   header.classList.toggle('header--js');
   nav.classList.toggle('nav--js');
@@ -33,14 +34,62 @@ cross.addEventListener('click', (e) =>{
 
 // Menu descriptions
 
-// const option = document.querySelectorAll('.eat__option--header');
-// const description = document.querySelector('.eat__description');
+const option = document.querySelectorAll('.eat__option--header');
+const description = document.querySelector('.eat__description');
 
+option.addEventListener('click', (e) =>{
+description.classList.toggle('eat__description--js');
+})
+
+
+// try1
 // option.forEach(('eat__option--header') =>{
 //   option.addEventListener('click',(e) =>{
 //     description.classList.toggle('eat__description--js')
 //   })
 // })
-// option.addEventListener('click', (e) =>{
-//   description.classList.toggle('eat__description--js');
+// try 2
+// document.addEventListener("DOMContentLoaded", function(){
+//   const option = document.querySelectorAll('.eat__option--header');
+//   const description = document.querySelector('.eat__description');
+
+//   option.addEventListener('click', (e) =>{
+//     description.classList.toggle('eat__description--js');
+//   })
 // })
+
+
+// const menuLink = document.querySelector('.nav__list--link-item');
+
+// menuLink.addEventListener('click', (e) =>{
+//   nav.classList.toggle('nav--js');
+//   header.classList.toggle('header--js');
+// })
+
+const home = document.getElementById("home");
+
+ home.addEventListener('click', (e) =>{
+  nav.classList.toggle('nav--js');
+  header.classList.toggle('header--js');
+ })
+
+const menu = document.getElementById("menu");
+
+menu.addEventListener('click', (e) =>{
+  nav.classList.toggle('nav--js');
+  header.classList.toggle('header--js');
+ })
+
+ const open = document.getElementById("open");
+
+ open.addEventListener('click', (e) =>{
+  nav.classList.toggle('nav--js');
+  header.classList.toggle('header--js');
+ })
+
+const contact = document.getElementById("contact");
+
+contact.addEventListener('click', (e) =>{
+  nav.classList.toggle('nav--js');
+  header.classList.toggle('header--js');
+ })
